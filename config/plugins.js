76ba789ff4a2b37a6349f01380ed114f3f365efa
abhinav <abhinavkr.2108@gmail.com>
2023-09-06 12:23:33 +0530

    Initial Commit

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dxjvnvywq',
+        api_key: '598477291386461',
+        api_secret: 'XZD6Gm-dcJhSYed77CBLavF84qA',
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
